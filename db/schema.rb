267f1b5a3b5b6d2d510e9f0effc2f7c08d381ac5
navaneedhan <navaneedhan.cit@gmail.com>
2019-01-20 15:01:17 +0530

    Adding tweets for the users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190120092505) do
+ActiveRecord::Schema.define(version: 20190120092836) do
+
+  create_table "tweets", force: true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tweets", ["user_id", "created_at"], name: "index_tweets_on_user_id_and_created_at"
 
   create_table "users", force: true do |t|
     t.string   "user"
