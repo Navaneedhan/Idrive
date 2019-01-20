96ffc4e7f484a609a7671598c5b8b2473c2d41b1
navaneedhan <navaneedhan.cit@gmail.com>
2019-01-20 14:43:11 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdriveApp::Application.config.secret_key_base = 'f36ec01389b0ee61726341c528297dfe31796c34fe7471c0f17b0d03ef756d91f9c7aa3699526f2a3ca8144a91d3e354d9ccf192279cb52a742044e1e114db95'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdriveApp::Application.config.session_store :cookie_store, key: '_idrive_app_session'
